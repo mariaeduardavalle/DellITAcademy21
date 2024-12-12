5ee95f3d3f97cd9e3ba26ed3ee82077f92f53245
Maria Eduarda Valle Santos <mevalle2004@gmail.com>
2024-12-12 13:46:19 -0300

    aula05

--- EntityFramework/Aula04e05/LojaApp/obj/Debug/net8.0/LojaApp.AssemblyInfo.cs
+++ EntityFramework/Aula04e05/LojaApp/obj/Debug/net8.0/LojaApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LojaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b44ee9bfaaea80baecf412b8a733b1ee475f046c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+313ef3959bb9a416f9b0dbe52b62b4c4cfceb886")]
 [assembly: System.Reflection.AssemblyProductAttribute("LojaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LojaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
